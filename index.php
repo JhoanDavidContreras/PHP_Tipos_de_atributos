470dabc59d218870fd00253d0e53b05ce69e6345
unknown <Jhoandavidcontreras14@gmail.com>
2020-12-02 20:06:56 -0500

    PHP_Tipos_de_Atributos -2-12-2020

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+class Verificar{
+    protected function Datos_A_Verificar($arg1,$arg2){
+        if($arg1=="jhoan"&& $arg2=="123"){
+            return "Usuario logrado";
+        }else{
+
+        }
+    }
+    class Login extends verificar{
+        private $usuario,$contranseña
+        public function _construct($arg1,$arg2){
+            $this->usuario-$arg1
+            $this->contraseña-$arg2
+        }
+    }
+
+    }
+    private function Enviar_Datos(){
+       return $this->Datos_A_Verificar($this->usuario, $this->contraseña);
+    }
+   public function mensaje(){
+       return $this->Datos_A_Verificar($this->usuario, $this->contraseña);
+   }
+   public function mensaje();
+   return $this Enviar_Mensaje();
+    }
+    
+}
+
+$obj = new Logim("Jhoan","123"),
+echo $obj ->mensaje();
+
+
+
+
+
+?>
